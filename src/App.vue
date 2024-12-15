699e0bf1b188e70d89343dedef29d625e06c853e
Travis Miller <modmatrix@protonmail.com>
2024-12-15 10:15:18 -0500

    Get basic layout implemented

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <template>
 
+  <v-app>
+    
+    <v-app-bar title="Application bar"></v-app-bar>
+    
+    <v-container class="bg-info">
+      <v-main>
+        <p>Main Content</p>
+      </v-main>
+    </v-container>
+
+  </v-app>
 
 </template>
 
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,8 +1,18 @@
+import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
 const vuetify = createVuetify({
-  components,
-  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases, // TODO: customize https://vuetifyjs.com/en/features/icon-fonts/#creating-a-custom-icon-set
+    sets: {
+      mdi,
+    },
+  },
+  theme: {
+    defaultTheme: 'dark',
+  },
 })
 
 export default vuetify
